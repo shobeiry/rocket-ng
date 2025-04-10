80cd209199975ec64b65f3d9818ad881861ceb75
Seyed Sadeq Shobeiry <ssshobeiry@gmail.com>
2025-04-10 13:32:19 +0330

    fix(checkbox): fix checkbox dark theme
    Author: sssh

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -28,7 +28,7 @@ export class CheckboxComponent implements ControlValueAccessor {
     "relative inline-flex items-center justify-center size-4 shrink-0 rounded border shadow-xs outline-none transition-shadow focus-visible:ring-[3px]",
     "focus-visible:ring-ring/50 border-input aria-checked:bg-primary aria-checked:text-primary-foreground aria-checked:border-primary aria-invalid:ring-destructive/20",
     "dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive disabled:cursor-not-allowed disabled:opacity-50 peer",
-    this.checked() ? 'bg-primary text-white border-primary' : "",
+    this.checked() ? 'bg-primary text-white border-primary dark:text-black' : "",
     this.disabled() ? 'cursor-not-allowed opacity-50' : "",
     this.class()
   ));
